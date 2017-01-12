76d2b86440f8b42a8e10e2b1958a4d0399bddf03
Tina La <tina.la47@gmail.com>
2017-01-12 11:46:00 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <errno.h>
+#include <sys/stat.h>
 
 #include "process.h"
 
 int main(int argc, char * argv[]){
+  // checks for file parameter
+  if (argc != 2) {
+    printf("clite2 requires exactly one file parameter\n");
+    exit(0);
+  }
+  
+  int fd = open(argv[1], O_CREAT | O_RDWR);
+  // prints command prompt
+  printf("> ");
+  
+  // 
+  char s[1000];
+  while(fgets(s, sizeof(s), stdin)) {
+    //s = format(s);
+    printf("> ");
+  }
+  
   return 0;
 }
