d62673b4ca3dec59bd5e7b10e4913693494c6e0a
Kevin Zhang <kevinzhang3702@gmail.com>
2017-01-17 21:14:58 -0500

    Added comments to functions

--- process.h
+++ process.h
@@ -1,9 +1,21 @@
+/*Struct For Storing a Single Entry in the Datatable. Because you are capable of storing different types of items, this is a workaround I have come up with
+ *If TYPE == TEXT:
+ *  TEXT_VAL is populated
+ *Else:
+ *  INT_VAL is populated
+ */
 struct data_entry {
   char * TYPE;
   int INT_VAL;
   char * TEXT_VAL;
 };
 
+/*Struct for a single table
+ *  TABLENAME : Name of the table. String.
+ *  HEADERS : Header for each column of the table. List of Strings
+ *  TYPES : Type for each column of the table. List of Strings
+ *  VALUES : Tangible data. Organized in rows, with each row containing data_entries. List of List of data_entry 
+ */
 struct data_table {
   char * TABLENAME;
   char * * HEADERS;
@@ -11,17 +23,52 @@ struct data_table {
   struct data_entry * * VALUES;
 };
 
+
+/*Struct for entire database in the file
+ *  TABLENAMES : List of tables by their names.
+ *  NUM_OF_TABLES (CURRENTLY NOT WORKING) : Number of tables in the database
+ *  DATATABLES : List of tables by their structs. 
+ */
 struct database{
   char * * TABLENAMES;
   int * NUM_OF_TABLES;
   struct data_table * DATATABLES;
 };
 
+/*Reads the database, given proper formatting ( see the file DBDenom ),  from the given file
+ *@params 
+ *  filename : file that is to be opened.
+ *@return
+ *  returns a database struct with all the necessary information ( see structs ). It is possible that there is data missing, such as in the case of an empty table, so it is wise to check.
+ */
 struct database * readDatabase(char * filename);
-void lock();
-void unlock();
-char * findStringPair( char ** originalString, char * firstDenom, char * secondDenom);
+
+/*Converts a properly formatted string ( see the file DBDenom ) into a struct data_table. Finds the table denomed by the tablename, and modifies the string.
+ *@params
+ *  fullString : address of the formatted string. The string will be modified as a result, leaving only the part AFTER the denom. 
+ *  tablename : name of the table you are looking for. NOTE: prior tables cannot be accessed after using this, so be careful of order.
+ *@return
+ *  returns a struct data_table
+ */
 struct data_table turnStringToTable(char ** fullString, char * tablename);
+
+/*Reverses the process done by turnStringToTable.
+ *@params
+ *  table : the table you are trying to convert
+ *@return
+ *  returns a properly formatted string ( see the file DBDenom ) that you can pop into a file. 
+ */
 char * turnTableToString( struct data_table table );
+
+/*Converts the entire database into a properly formatted string ( see the file DBDenom ) and writes it to the file. Note : the file is overwritten. All previous data is lost. 
+ *@params
+ *  db : the database you are trying to convert
+ *  filename : the name of the file you are writing to
+ */
 void writeDatabase( struct database db, char * filename );
+
+/*Creates a file if it does not exist. Perms are 0644. 
+ *@params
+ *  filename : name of the file. 
+ */
 void createFileIfNotExists( char * filename );
--- utils.h
+++ utils.h
@@ -1,9 +1,49 @@
-extern struct database;
-extern struct data_table;
+extern struct database; // See process.h for details 
+extern struct data_table; // See process.h for details. 
 
+/*Searches the beginning and end of originalString for whiteSpace characters ( ' ', and '\n' being two examples ) and removes them. The originalString is modified as a result.
+ *@params
+ *  originalString : address of the string you are trying to modify.
+ *@return
+ *  returns a copy of the originalString, in case you want to use it for something else. Note : the originalString is still modified. 
+ */
 char * stripWhiteSpace( char ** originalString );
-char * findStringPair( char ** originalString, char * firstDenom, char * secondDenom);
+
+/*Pairs firstDenom with secondDenom in a given originalString, and returns everything in between. The originalString is modified as a result.
+ *@params
+ *  originalString : the address of the string you are searching through for.
+ *  firstDenom : string of the opening part of what you are looking for.
+ *  secondDenom : string of the ending part of what you are looking for.
+ *@return
+ *  returns the string between the denoms. If not found, it returns an error.
+ *Example of use:
+ *   char * string = "( > . < ) HAHAHA ";
+ *   findStringPair( &string, "(", ")" );
+ *      - returns " > . < ";
+ *      - string becomes " HAHAHA ";
+ */
+char * findStringPair( char ** originalString, char * firstDenom, char * secondDenom );
+
+/*Locks. Use whenever you need to access the file.
+ */
 void lock();
+
+/*Unlocks. Use when you are done using the file.
+ */
 void unlock();
+
+/*Prints the chosen tables from the database in proper SQlite3 format.
+ *@params
+ *  db : the entire database
+ *  listOfTableNames : List of tables that you are printing out. 
+ *  numOfTables : For ease of looping. The number of tables you are printing out.
+ *@return
+ *  prints out the database.
+ */
 void printDatabase( struct database * db, char ** listOfTableNames, int numOfTables );
+
+/*Print a single table 
+ *@params
+ *  tb : the table
+ */
 void printTable( struct data_table * tb );
